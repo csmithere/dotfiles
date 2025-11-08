ff1e77c633fda63f9d5d94fc0a9ff36e896cf177
Christopher M. Smith <csmith@bigid.com>
2025-11-08 16:23:24 -0500

    updates to lsp plugins for LuaSnip

--- private_dot_config/private_nvim/private_lua/private_plugins/lsp.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/lsp.lua
@@ -7,14 +7,8 @@ return {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
     },
     config = function()
-      local cmp = require("cmp")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Setup mason
@@ -27,28 +21,6 @@ return {
           "marksman",  -- Markdown LSP
           "yamlls"     -- YAML LSP
         },
-      })
-
-      -- Setup nvim-cmp
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        })
       })
 
       -- LSP keybindings callback
