c582be8085c9bc186f475cbfe7173b2dcf84b786
Christopher M. Smith <csmith@bigid.com>
2025-11-08 14:51:56 -0500

    Starship prompt + fish configuration mod

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,17 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# AWS
-#export AWS_PROFILE=presales
-# AWS cli completion
-test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-# General Shell Functions
+# General
 alias ls="/opt/homebrew/bin/lsd"
 alias vi="/opt/homebrew/bin/nvim"
 alias vim="/opt/homebrew/bin/nvim"
 alias cat="/opt/homebrew/bin/bat"
 alias grep="/opt/homebrew/bin/rg"
 set -gx EDITOR nvim
+
+# thef__k
+thefuck --alias | source
+
+# AWS
+#export AWS_PROFILE=presales
+# AWS cli completion
+test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
 # SSH SOCK
 set -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
